8969659d7dc83463386adfc50e2088866d545720
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 08:49:34 -0300

    ss

---
+++ js.js
@@ -0,0 +1,17 @@
+const animals = ["lobo", "tucano", "elefante", "pavão", "raposa", "leão"];
+
+
+if (!animals.includes("leão")) {
+  console.log("Leão não existe no array animals.");
+} else {
+  console.log("Existe um leão no array animals.");
+}
+
+const animals = ["lobo", "tucano", "elefante", "pavão", "raposa", "leão"];
+
+
+if (!animals.includes("leão")) {
+  console.log("Leão não existe no array animals.");
+} else {
+  console.log("Existe um leão no array animals.");
+}
